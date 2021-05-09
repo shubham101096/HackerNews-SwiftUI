ab0f99e435e95c10936935859ea0742df40875b2
Shubham Mishra <shubhammishra10@gmail.com>
2021-05-09 09:26:05 +0530

    Initial Commit

---
+++ HackNewsSwiftUI/HackNewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackNewsSwiftUIApp.swift
+//  HackNewsSwiftUI
+//
+//  Created by Shubham Mishra on 09/05/21.
+//
+
+import SwiftUI
+
+@main
+struct HackNewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
